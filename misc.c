951074ba710a9d4d5ba998c4cc2975084d23ecc5
gamemoddesignbr <gamemoddesignbr@gmail.com>
2024-10-05 12:31:48 -0300

    Changing LED color to green

--- misc.c
+++ misc.c
@@ -78,8 +78,8 @@ void halt_with_error(uint32_t err, uint32_t bits)
             bool is_long = err & (1 << (bits - i - 1));
             sleep_ms(is_long ? LONG_PAUSE_TIME : SHORT_PAUSE_TIME);
             bool success = bits == 1 && is_long == 0;
-            if (success)
-                put_pixel(PIX_whi);
+            if ((success) || (err == 0)) //small change for lighting green only when a successful glitch happens
+                put_pixel(PIX_gre);
             else
                 put_pixel(PIX_yel);
             sleep_ms(is_long ? LONG_TIME : success ? SHORT_TIME * 2 : SHORT_TIME);
